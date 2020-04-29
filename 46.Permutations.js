7a0386147ec8e0882a2ae15e6620d5bcf636893e
SuSunSam <339341037@qq.com>
2020-04-29 18:13:47 +0800

    4.29

--- 46.Permutations.js
+++ 46.Permutations.js
@@ -16,8 +16,3 @@ var permute = function(nums) {
       dfs(0);
       return ans;
   };
-  
-  作者：Sun2ama
-  链接：https://juejin.im/post/5ea40b2af265da47cb3130ad
-  来源：掘金
-  著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
