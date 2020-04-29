0834063bf878472dd39fc7cf9b1dd37faf6ff889
SuSunSam <339341037@qq.com>
2020-04-29 18:17:06 +0800

    4.29

--- 46.Permutations.js
+++ 46.Permutations.js
@@ -1,18 +1,18 @@
 var permute = function(nums) {
     if(nums==0) return [];
-      var len=nums.length,used=[],path=[],ans=[];
-      var dfs = function(d){
-          if(d==len){ans.push([...path]);//拷贝}
-          for(var i=0;i<len;i++){
-              if(!used[nums[i]]){//该数字还没被加入path
+    var len=nums.length,used=[],path=[],ans=[];
+    var dfs = function(d){
+        if(d==len){ans.push([...path]);}//拷贝
+        for(var i=0;i<len;i++){
+            if(!used[nums[i]]){//该数字还没被加入path
                   path.push(nums[i]);
                   used[nums[i]] = true;
                   dfs(d+1);
                   path.pop();
                   used[nums[i]] = false;
-              }
-          }
-      }
-      dfs(0);
-      return ans;
-  };
+            }
+        }
+    }
+    dfs(0);
+    return ans;
+}
